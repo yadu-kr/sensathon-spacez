401b8218831909a555675b56f55b2611e702607b
Danny Racho <64934810+Dannyracho1@users.noreply.github.com>
2023-06-24 12:19:55 +0200

    Game commented und documented
    ready to submit

--- FLAPPY/game.py
+++ FLAPPY/game.py
@@ -1,10 +1,98 @@
+"""
+Code from https://github.com/kodexxx/flappy-bird
+
+was used with the DPS368 (Kit 2Go) Digital Pressure Sensors.
+The pressure measurements (in command mode) are received via serial port
+and processed to determine the height of the flappy bird
+
+Modified by Space-Z, 24.06.2023
+Infineon and BEST Aachen hosted Sensathon
+"""
 from tkinter import *
+import time
+import serial
+import threading
+import continuous_threading
 import random
 import os
 
+
+ser1 = serial.Serial('COM8',19200) #refernce
+ser2 = serial.Serial('COM9',19200) #joystick
+val1 = 0
+
+# Auxiliary sensor as a reference
+index1 = 0
+data1 = 0
+# Main sensor to play
+index2 = 0
+data2 = 0
+
+# ignore first received data, in case corrupted 
+counter = 0
+# intial difference between sensor's position
+calib_value = 0
+# limits
+lower = 0
+upper = 0
+# determined mapped value
+newvalue = 0
+
+def readserial():
+    
+    global counter
+    global calib_value
+    global lower
+    global upper
+    global newvalue
+
+    # read from serial line & store in variable data
+    index1 = ser1.readline().decode('utf-8')
+    data1 = float(index1.rstrip())
+    #print ("Ground", data1)
+    index2 = ser2.readline().decode('utf-8')
+    data2 = float(index2.rstrip())
+    #print ("Controller", data2)
+    
+
+    if (counter == 0):
+        counter = counter + 1
+    elif (counter == 1):
+        counter = counter + 1
+        calib_value = data1 - data2
+        #print("Calib value", calib_value)
+
+    lower = calib_value - 15
+    upper = calib_value + 15
+    oldvalue = data1-data2
+        
+    if ((data1-data2) > calib_value):
+        #print("Up")
+        # map data difference to value between 0 and 350      
+        newvalue = ((oldvalue - calib_value) * 350)/(upper - calib_value)
+        #main.after(FRAMERATE, birdUp)
+        birdUp()
+        #print("New value", newvalue)
+
+    elif ((data1-data2) <= calib_value):
+        #print("Down")
+        # map data diff to value between 0 and 350        
+        newvalue = 350 - ((oldvalue - lower) * 350)/(calib_value - lower)
+        #main.after(FRAMERATE, birdDown)
+        birdDown()
+        #print("New value", newvalue)
+
+    #print("Nothing true: ", data1, "  ", data2)
+
+
+t1 = continuous_threading.PeriodicThread(0.05, readserial)
+t1.start()
+
+# framerate of the game
 FRAMERATE = 20
 SCORE = -1
 
+# keep flappy bird centered
 def center(toplevel):
 	toplevel.update_idletasks()
 	w = toplevel.winfo_screenwidth()
@@ -13,21 +101,22 @@ def center(toplevel):
 	x = w/2 - size[0]/2
 	y = h/2 - size[1]/2 - 35
 	toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
-	
+
 main = Tk()
 main.resizable(width = False, height = False)
-main.title("Flappy Bird")
-main.geometry('550x700')
+main.title("Flappy Bird")   # title
+main.geometry('550x700')    # window size
 
 center(main)
 
-BIRD_Y = 200
+# initial values for main loop
+BIRD_Y = 350
 PIPE_X = 550
 PIPE_HOLE = 0
 NOW_PAUSE = False
 
 BEST_SCORE = 0
-
+# kepp track of best score in a saved file
 if os.path.isfile("data.dat"):
 	scoreFile = open('data.dat')
 	BEST_SCORE = int(scoreFile.read())
@@ -45,7 +134,7 @@ bird = w.create_image(100, BIRD_Y, image=birdImg)
 
 up_count = 0
 endRectangle = endBest = endScore = None
-
+# create pipes as obstacles
 pipeUp = w.create_rectangle(PIPE_X, 0, PIPE_X + 100, PIPE_HOLE, fill="#74BF2E", outline="#74BF2E")
 pipeDown = w.create_rectangle(PIPE_X, PIPE_HOLE + 200, PIPE_X + 100, 700, fill="#74BF2E", outline="#74BF2E")
 score_w = w.create_text(15, 45, text="0", font='Impact 60', fill='#ffffff', anchor=W)
@@ -64,32 +153,32 @@ def generatePipeHole():
 
 generatePipeHole()
 
-def birdUp(event = None):
-	global BIRD_Y
-	global up_count
-	global NOW_PAUSE
-	
-	if NOW_PAUSE == False: 
-		BIRD_Y -= 20
-		if BIRD_Y <= 0: BIRD_Y = 0
-		w.coords(bird, 100, BIRD_Y)
-		if up_count < 5:
-			up_count += 1
-			main.after(FRAMERATE, birdUp)
-			
-		else: up_count = 0
-	else:
-		restartGame()
+# make bird go up
+def birdUp():
+    global BIRD_Y
+    global up_count
+    global NOW_PAUSE
+    global newvalue
+    
+    BIRD_Y = 350 - int(newvalue)
+    #print(BIRD_Y)
+    if BIRD_Y <= 0: BIRD_Y = 0
+    w.coords(bird, 100, BIRD_Y)
 
+
+# make bird go down
 def birdDown():
-	global BIRD_Y
-	global NOW_PAUSE
+    global BIRD_Y
+    global NOW_PAUSE
+    global newvalue
+    
+    BIRD_Y = 350 + int(newvalue)
+    #print(BIRD_Y)
+    if BIRD_Y >= 700: BIRD_Y = 700
+    w.coords(bird, 100, BIRD_Y)
 
-	BIRD_Y += 8
-	if BIRD_Y >= 700: BIRD_Y = 700
-	w.coords(bird, 100, BIRD_Y)
-	if NOW_PAUSE == False: main.after(FRAMERATE,birdDown)
 
+# side scroll the pipes as obstacles
 def pipesMotion():
 	global PIPE_X
 	global PIPE_HOLE
@@ -105,14 +194,17 @@ def pipesMotion():
 	
 	if NOW_PAUSE == False: main.after(FRAMERATE, pipesMotion)
 
-def engGameScreen():
-	global endRectangle
-	global endScore
-	global endBest
-	endRectangle = w.create_rectangle(0, 0, 550, 700, fill='#4EC0CA')
-	endScore = w.create_text(15, 200, text="Your score: " + str(SCORE), font='Impact 50', fill='#ffffff', anchor=W)
-	endBest = w.create_text(15, 280, text="Best score: " + str(BEST_SCORE), font='Impact 50', fill='#ffffff', anchor=W)
+# show current score and best score
+def endGameScreen():
+    global endRectangle
+    global endScore
+    global endBest
 
+    endRectangle = w.create_rectangle(0, 0, 550, 700, fill='#4EC0CA')
+    endScore = w.create_text(15, 200, text="Your score: " + str(SCORE), font='Impact 50', fill='#ffffff', anchor=W)
+    endBest = w.create_text(15, 280, text="Best score: " + str(BEST_SCORE), font='Impact 50', fill='#ffffff', anchor=W)
+
+# pause game when a pipe is hit
 def detectCollision():
 	global NOW_PAUSE
 	global BEST_SCORE
@@ -126,31 +218,36 @@ def detectCollision():
 			scoreFile.write(str(BEST_SCORE))
 			scoreFile.close()
 		#print("Pause")
-		engGameScreen()
+		endGameScreen()
 	if NOW_PAUSE == False: main.after(FRAMERATE, detectCollision)
 
-def restartGame():
-	global PIPE_X
-	global BIRD_Y
-	global SCORE
-	global NOW_PAUSE
-	global FRAMERATE
+# reset counter to zero to re-calibrate joystick sensor to reference sensor
+def restartGame(event = None):
+    global PIPE_X
+    global BIRD_Y
+    global SCORE
+    global NOW_PAUSE
+    global FRAMERATE
+    
+    BIRD_Y = 350
+    PIPE_X = 550
+    SCORE = -1
+    FRAMERATE = 20
+    NOW_PAUSE = False
+    
+    global counter
+    
+    counter = 0
 
-	BIRD_Y = 200
-	PIPE_X = 550
-	SCORE = -1
-	FRAMERATE = 20
-	NOW_PAUSE = False
-	w.delete(endScore)
-	w.delete(endRectangle)
-	w.delete(endBest)
-	generatePipeHole()
-	main.after(FRAMERATE, birdDown)
-	main.after(FRAMERATE, pipesMotion)
-	main.after(FRAMERATE, detectCollision)
-	
-main.after(FRAMERATE, birdDown)
+    w.delete(endScore)
+    w.delete(endRectangle)
+    w.delete(endBest)
+    generatePipeHole()
+    main.after(FRAMERATE, pipesMotion)
+    main.after(FRAMERATE, detectCollision)
+
 main.after(FRAMERATE, pipesMotion)
 main.after(FRAMERATE, detectCollision)
-main.bind("<space>", birdUp)
+main.bind("<space>", restartGame)
+
 main.mainloop()
